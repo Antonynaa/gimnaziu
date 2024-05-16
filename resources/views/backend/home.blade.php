bcf40d4738c4896dac063b7a36e6ad1237a4be41
Cojocari Antonina <cojocari.antonina@elev.cihcahul.md>
2024-05-16 13:35:13 +0300

    Panou de control

---
+++ resources/views/backend/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', env('APP_NAME', 'AdminLTE'))
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+<p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css') 
+
+@stop
+
+@section('js')
+@vite('resources/js/app.js')
+@stop
